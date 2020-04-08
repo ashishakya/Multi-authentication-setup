a79d9e3d9c0f78cb41efbb6fc3f2c48bcadb5ef8
Ashish Shakya <ashish.shakya@yipl.com.np>
2020-04-08 14:16:03 +0545

    Initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Notifications\AdminResetPasswordNotification;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin.home');
+    }
+
+}
